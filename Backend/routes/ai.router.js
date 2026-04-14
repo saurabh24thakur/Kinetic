bdb879dd18cb8b08b680427a54867e3241d626c4
Saurabh Singh <saurabh5532u@gmail.com>
2026-04-14 18:27:04 +0530

    Delete Backend directory

--- Backend/routes/ai.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { generateCode } = require("../controller/ai.controller");
-const { requireAuth } = require("../middleware/auth.middleware");
-
-const router = express.Router();
-
-router.post("/generate", requireAuth, generateCode);
-
-module.exports = router;
